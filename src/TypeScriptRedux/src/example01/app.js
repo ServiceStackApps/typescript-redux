762516fda5995bc1ac0a708ac21d4097e923932e
Demis Bellot <demis.bellot@gmail.com>
2016-01-19 17:13:42 -0500

    Add HelloWorld example01

---
+++ src/TypeScriptRedux/src/example01/app.js
@@ -0,0 +1,33 @@
+/// <reference path='../../typings/tsd.d.ts'/>
+System.register(['react', 'react-dom'], function(exports_1) {
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+    var React, ReactDOM;
+    var HelloWorld;
+    return {
+        setters:[
+            function (React_1) {
+                React = React_1;
+            },
+            function (ReactDOM_1) {
+                ReactDOM = ReactDOM_1;
+            }],
+        execute: function() {
+            HelloWorld = (function (_super) {
+                __extends(HelloWorld, _super);
+                function HelloWorld() {
+                    _super.apply(this, arguments);
+                }
+                HelloWorld.prototype.render = function () {
+                    return (React.createElement("div", null, "Hello, World!"));
+                };
+                return HelloWorld;
+            })(React.Component);
+            ReactDOM.render(React.createElement(HelloWorld, null), document.getElementById("content"));
+        }
+    }
+});
+//# sourceMappingURL=app.js.map
---
+++ src/TypeScriptRedux/src/example01/app.tsx
@@ -0,0 +1,18 @@
+﻿/// <reference path='../../typings/tsd.d.ts'/>
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class HelloWorld extends React.Component<any, any> {
+    render() {
+        return (
+            <div>Hello, World!</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById("content"));
+
+ 
