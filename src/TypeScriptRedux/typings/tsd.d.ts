6466aa4de0b5476c97b8470f35078adad939d5f0
Demis Bellot <demis.bellot@gmail.com>
2016-01-19 23:54:06 -0500

    tsd install es6-shim --save

--- src/TypeScriptRedux/typings/tsd.d.ts
+++ src/TypeScriptRedux/typings/tsd.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="redux/redux.d.ts" />
 /// <reference path="react-redux/react-redux.d.ts" />
+/// <reference path="es6-shim/es6-shim.d.ts" />
