56da95026b775b73e5e39d1081e3b91cf1987f09
Demis Bellot <demis.bellot@gmail.com>
2016-01-19 23:40:54 -0500

    Add Example 5 - Use react-redux Provider to inject store into child components

---
+++ src/TypeScriptRedux/src/example05/Counter.js
@@ -0,0 +1,41 @@
+/// <reference path='../../typings/tsd.d.ts'/>
+System.register(['react'], function(exports_1) {
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+    var React;
+    var Counter;
+    return {
+        setters:[
+            function (React_1) {
+                React = React_1;
+            }],
+        execute: function() {
+            Counter = (function (_super) {
+                __extends(Counter, _super);
+                function Counter() {
+                    _super.apply(this, arguments);
+                }
+                Counter.prototype.componentDidMount = function () {
+                    var _this = this;
+                    this.unsubscribe = this.context.store.subscribe(function () { return _this.forceUpdate(); });
+                };
+                Counter.prototype.componentWillUnmount = function () {
+                    this.unsubscribe();
+                };
+                Counter.prototype.render = function () {
+                    var _this = this;
+                    return (React.createElement("div", null, React.createElement("p", null, React.createElement("label", null, "Counter: "), React.createElement("b", null, "#", this.context.store.getState().counter)), React.createElement("button", {"onClick": function (e) { return _this.context.store.dispatch({ type: 'INCR', by: 1 }); }}, "INCREMENT"), React.createElement("span", {"style": { padding: "0 5px" }}), React.createElement("button", {"onClick": function (e) { return _this.context.store.dispatch({ type: 'INCR', by: -1 }); }}, "DECREMENT")));
+                };
+                Counter.contextTypes = {
+                    store: React.PropTypes.object
+                };
+                return Counter;
+            })(React.Component);
+            exports_1("default", Counter);
+        }
+    }
+});
+//# sourceMappingURL=Counter.js.map
---
+++ src/TypeScriptRedux/src/example05/Counter.tsx
@@ -0,0 +1,33 @@
+﻿/// <reference path='../../typings/tsd.d.ts'/>
+
+import * as React from 'react';
+
+export default class Counter extends React.Component<any, any> {
+
+    context: any;
+    static contextTypes = {
+        store: React.PropTypes.object
+    }
+
+    private unsubscribe: Function;
+
+    componentDidMount() {
+        this.unsubscribe = this.context.store.subscribe(() => this.forceUpdate());
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    <label>Counter: </label><b>#{this.context.store.getState().counter}</b>
+                </p>
+                <button onClick={e => this.context.store.dispatch({ type:'INCR', by: 1 }) }>INCREMENT</button>
+                <span style={{ padding: "0 5px" }} />
+                <button onClick={e => this.context.store.dispatch({ type:'INCR', by: -1 }) }>DECREMENT</button>
+            </div>
+        );
+    }
+}
