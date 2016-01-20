713d7a34dd62b444eb2a177c651badbc70442158
Demis Bellot <demis.bellot@gmail.com>
2016-01-19 21:20:31 -0500

    tsd install react-redux --save

--- src/TypeScriptRedux/typings/tsd.d.ts
+++ src/TypeScriptRedux/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="redux/redux.d.ts" />
+/// <reference path="react-redux/react-redux.d.ts" />
