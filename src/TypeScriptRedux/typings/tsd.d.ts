a759b2766d64c3f92265e281a7c7fc3d38a828ae
Demis Bellot <demis.bellot@gmail.com>
2016-02-06 10:39:17 -0500

    Upgrade tsd

--- src/TypeScriptRedux/typings/tsd.d.ts
+++ src/TypeScriptRedux/typings/tsd.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="redux/redux.d.ts" />
 /// <reference path="react-redux/react-redux.d.ts" />
 /// <reference path="es6-shim/es6-shim.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="ss-utils/ss-utils.d.ts" />
